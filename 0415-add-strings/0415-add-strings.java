0b072751012e2be6498caf49cdffbda28313860d
ThePrinceM <159701303+ThePrinceM@users.noreply.github.com>
2025-12-11 00:12:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        long n1=Long.parseLong(num1);
+        long n2=Long.parseLong(num2);
+        return Long.toString(n1+n2);
+    }
+}
