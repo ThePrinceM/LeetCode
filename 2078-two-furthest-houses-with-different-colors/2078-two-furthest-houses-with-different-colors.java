dcc4f55e2eb36fc8bb2d2ea896373b2d6e17c3f6
Prince Jeetlal Maurya <159701303+ThePrinceM@users.noreply.github.com>
2026-04-20 21:10:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -3,13 +3,11 @@ class Solution {
         int n = colors.length;
         int i = 0, j = n - 1;
 
-        // First, check from the leftmost house to the rightmost different color
         while (colors[i] == colors[j]) {
             j--;
         }
         int maxDist1 = j - i;
 
-        // Then, check from the rightmost house to the leftmost different color
         j = n - 1;
         while (colors[i] == colors[j]) {
             i++;
