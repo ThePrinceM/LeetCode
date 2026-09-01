d1c60019c81e9c706e6c9ece1ff64df5a89b0c6e
Prince Jeetlal Maurya <159701303+ThePrinceM@users.noreply.github.com>
2026-09-01 23:22:03 +0530

    Time: 0 ms (100%), Space: 45.1 MB (66.16%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,29 +14,30 @@
  * }
  */
 class Solution {
+    static boolean flag;
 
-    public int minNode(TreeNode root){
-        if (root == null) return Integer.MAX_VALUE;
-        if (root.left == null) return root.val;
-        return minNode(root.left);
+    private long max(TreeNode root) {
+        if(root == null) return Long.MIN_VALUE;
+        long leftMax = max(root.left);
+        if(leftMax >= root.val) flag = false;
+        long rightMax = max(root.right);
+        return Math.max(root.val, Math.max(leftMax, rightMax));
     }
 
-    public int maxNode(TreeNode root){
-        if (root == null) return Integer.MIN_VALUE;
-        if (root.right == null) return root.val;
-        return maxNode(root.right);
+    private long min(TreeNode root) {
+        if(root == null) return Long.MAX_VALUE;
+        long leftMin = min(root.left);
+        long rightMin = min(root.right);
+        if(rightMin <= root.val) flag = false;
+        return Math.min(root.val, Math.min(leftMin, rightMin));
     }
 
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-
-        // check that all nodes in left subtree are < root.val
-        if (root.left != null && maxNode(root.left) >= root.val) return false;
-
-        // check that all nodes in right subtree are > root.val
-        if (root.right != null && minNode(root.right) <= root.val) return false;
-
-        // recursively ensure left and right subtrees are valid BSTs
-        return isValidBST(root.left) && isValidBST(root.right);
+        flag = true;
+        max(root);
+        min(root);
+        return flag;
     }
 }
+
+
